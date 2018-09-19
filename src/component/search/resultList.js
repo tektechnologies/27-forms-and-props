50131f452cb8fb27fb59acf2e609e788b9a1590a
tektechnologies <info@tektechnologies.com>
2018-09-18 19:46:29 -0500

    add result list

--- src/component/search/resultList.js
+++ src/component/search/resultList.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 
 export default class List extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className='searchResults'>
+                <ul>
+                    {this.props.topics.map((topic, i) =>
+                        <li key={i}>{topic}</li>
+                        )}
+                </ul>
+            </div>
+        );
+    }
 
 
 
 
-    
+
 }
